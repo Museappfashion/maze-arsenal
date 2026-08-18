b7f18c4cc2865c48d04790390922836aaad908a6
Museappfashion <sdp100@aol.com>
2026-08-18 19:43:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-// src/main.jsx
-
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { createRoot } from "react-dom/client";
